f47ca351eb5ad8220ce10c1cd7da670f64855a8c
azhrzf <a.zaidan.f.azf616@gmail.com>
2022-08-24 16:33:12 +0700

    update su

---
+++ src/Components/Header/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Navbar = () => {
+    return (
+        <header className="navbar">
+            <p>Pembuat Daftar Pustaka</p>
+            <img alt="logoGithub" src="https://cdn-icons-png.flaticon.com/512/25/25231.png"></img>
+        </header>
+    )
+}
+
+export default Navbar
